f4227bdab254ccc84e01b5ac183630480cced5c2
Bodey Baker <bodeybaker@gmail.com>
2025-07-18 19:25:37 -0600

    Use relative address for manifest file

--- bin/regenerate_index.py
+++ bin/regenerate_index.py
@@ -297,7 +297,7 @@ def get_html() -> str:
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="manifest" href="manifest.json" />
+  <link rel="manifest" href="./manifest.json" />
   <link rel="stylesheet" href="https://public.codepenassets.com/css/reset-2.0.min.css">
   <link rel="stylesheet" href="./css/layout.css">
   <link rel="stylesheet" href="./css/typography.css">
