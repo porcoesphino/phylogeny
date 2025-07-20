09aa607e444b95c9e784821e5c7b9410b9ad4f1e
Bodey Baker <bodeybaker@gmail.com>
2025-07-20 16:35:25 -0500

    Ensure initial page loads with query params still hit the cache

--- cacher.js
+++ cacher.js
@@ -21,11 +21,16 @@ function url_is_remote(url) {
   return url.startsWith('https://porcoesphino.github.io')
 }
 
+function is_remote(event) {
+  const scope = event.target.registration.scope
+  return url_is_remote(scope)
+}
+
 function url_is_favicon(url) {
   return url.endsWith('favicon_tree_192.png') || url.endsWith('favicon_tree_512.png')
 }
 
-async function match_from_cache_and_fetch_on_miss(cache_name, url_or_request, put_on_success = true) {
+async function cache_match_with_fetch_fallback(cache_name, url_or_request, put_on_success = true) {
   const cache = await caches.open(cache_name)
   if (typeof (url_or_request) == 'string') {
     var request = new Request(url_or_request)
@@ -47,22 +52,22 @@ async function match_from_cache_and_fetch_on_miss(cache_name, url_or_request, pu
   return fetch_response
 }
 
-async function fault_tolerant_add_all(cache_name, list_or_set, only_if_cache_miss = false) {
+async function fault_tolerant_add_all(cache_name, list_or_set, only_add_on_cache_miss = false) {
   const cache = await caches.open(cache_name)
   url_list = [...list_or_set]
   for (var i = 0; i < url_list.length; i++) {
     var url = url_list[i]
-    if (only_if_cache_miss) {
-      await match_from_cache_and_fetch_on_miss(cache_name, url, put_on_success = true)
+    if (only_add_on_cache_miss) {
+      await cache_match_with_fetch_fallback(cache_name, url, put_on_success = true)
     } else {
       await cache.add(url)
     }
   }
-  console.log(`Finished ensuring the cache is fresh for ${url_list.length} items.only_if_cache_miss = (${only_if_cache_miss})`, url_list)
+  console.log(`Finished ensuring the cache is fresh for ${url_list.length} items. (only_add_on_cache_miss = ${only_add_on_cache_miss})`, url_list)
 }
 
 async function precache(event) {
-  if (url_is_remote(event.target.registration.scope)) {
+  if (is_remote(event)) {
     var prefix = '/phylogeny'
   } else {
     var prefix = ''
@@ -71,7 +76,31 @@ async function precache(event) {
   for (var i = 0; i < precached_resources.length; i++) {
     initial_load.push(prefix + precached_resources[i])
   }
-  return fault_tolerant_add_all(cache_name_versioned, initial_load, false /* only_if_cache_miss */)
+  return fault_tolerant_add_all(cache_name_versioned, initial_load, false /* only_add_on_cache_miss */)
+}
+
+function is_request_for_website(request) {
+  // This is the initial page load.
+  if (request.referrer == '') {
+    return true
+  }
+  const referrer_url = new URL(request.referrer)
+  const request_url = new URL(request.url)
+
+  return referrer_url.host = request_url.host
+}
+
+function has_query_params(request) {
+  const request_url = new URL(request.url)
+  return request_url.search.length > 0
+}
+
+function strip_query_params(url_str) {
+  const url = new URL(url_str)
+  for (const key of url.searchParams.keys()) {
+    url.searchParams.delete(key)
+  }
+  return url
 }
 
 self.addEventListener('install', (event) => {
@@ -86,23 +115,27 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
 
-  const url = event.request.url
-
   // The server puts files in a directory and so the HTML needs to reference this directory
   // but this doesn't align with local dev. For local dev catch and reroute these requests.
   var updated_request = event.request.clone()
-  if (url_is_favicon(url) && !url_is_remote(url)) {
-    split_url = event.request.url.split('/')
+  const original_url = updated_request.url
+  if (url_is_favicon(original_url) && !url_is_remote(original_url)) {
+    split_url = original_url.split('/')
     updated_request = new Request('./' + split_url[split_url.length - 1])
+
+    // If this is request for this website, and has quer params, then strip them.
+    // The response will be the same and it will mean there are more cache hits.
+  } else if (is_request_for_website(event.request) && has_query_params(event.request)) {
+    updated_request = new Request(strip_query_params(original_url))
   }
 
-  if (url.includes('/thumbnails/')) {
-    event.respondWith(match_from_cache_and_fetch_on_miss(cache_name_thumbnails, updated_request, true /* put_on_success */))
+  if (is_request_for_website(event.request) && original_url.includes('/thumbnails/')) {
+    event.respondWith(cache_match_with_fetch_fallback(cache_name_thumbnails, updated_request, true /* put_on_success */))
   } else {
-    event.respondWith(match_from_cache_and_fetch_on_miss(cache_name_versioned, updated_request, false /* put_on_success */))
+    event.respondWith(cache_match_with_fetch_fallback(cache_name_versioned, updated_request, false /* put_on_success */))
   }
 });
 
 self.addEventListener('message', async (event) => {
-  event.waitUntil(fault_tolerant_add_all(cache_name_thumbnails, event.data, true /* only_if_cache_miss */))
+  event.waitUntil(fault_tolerant_add_all(cache_name_thumbnails, event.data, true /* only_add_on_cache_miss */))
 });
